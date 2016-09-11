c6f868a034da06c5b6f5281177f0061c754bd5e0
Kyle E. Mitchell <kyle@kemitchell.com>
2016-09-11 11:09:46 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var readability = require('readability-api')
+var stringify = require('lispy-json')
+
+readability.configure({
+  consumer_key: process.env.READABILITY_KEY,
+  consumer_secret: process.env.READABILITY_SECRET
+})
+
+readability.xauth(
+  process.env.READABILITY_USER,
+  process.env.READABILITY_PASSWORD,
+  function (error, tokens) {
+    var page = 0
+    var reader = new readability.reader({
+      access_token: tokens.oauth_token,
+      access_token_secret: tokens.oauth_token_secret
+    })
+    printPage(1)
+    function printPage (page) {
+      reader.bookmarks({
+        page: page,
+        per_page: 50
+      }, function (error, bookmarks) {
+        if (error) {
+          console.error(error)
+          process.exit(1)
+        } else {
+          console.error('Read page ' + page)
+          console.log(stringify(bookmarks))
+          var pages = bookmarks.meta.num_pages
+          if (page === pages) {
+            process.exit(0)
+          } else {
+            printPage(page + 1)
+          }
+        }
+      })
+    }
+  }
+)
+
